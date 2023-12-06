41fbbf21b42fac7c41b5f8c3a3bee1bd084a6ba2
muhammadmesum72 <me.m.mesum@gmail.com>
2023-12-06 18:18:53 +0500

    v-2

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,54 +1,30 @@
-import Nft1 from "./Nft (1).jpg";
-import Nft2 from "./Nft (2).jpg";
-import Nft3 from "./Nft (3).jpg";
-import Nft4 from "./Nft (4).jpeg";
-
-export const walletNfts = [
+export const navLinks = [
   {
-    nft: Nft1,
-    id: 5681,
+    name: "Home",
+    path: "/",
+    id: 1,
   },
   {
-    nft: Nft2,
-    id: 5382,
+    name: "Mint",
+    path: "/mint",
+    id: 2,
   },
   {
-    nft: Nft3,
-    id: 5856,
-  },
-  {
-    nft: Nft4,
-    id: 5443,
+    name: "NFT viewer",
+    path: "/nftviewer",
+    id: 4,
   },
 ];
 
-export const navLinks = [
-    {
-      name: "Home",
-      path: "/",
-      id :1
-    },
-    {
-      name: "Mint",
-      path: "/mint",
-      id: 2
-    },
-    {
-      name: "NFT viewer",
-      path: "/nftviewer",
-      id: 4
-    },
-  ];
-
-  export const socialLinks = [
-    {
-        name: "Discord",
-        path : "https://www.discord.com",
-        id: 1
-    },
-    {
-        name: "Twitter",
-        path : "https://www.twitter.com",
-        id: 2
-    },
-  ]
+export const socialLinks = [
+  {
+    name: "Discord",
+    path: "https://www.discord.com",
+    id: 1,
+  },
+  {
+    name: "Twitter",
+    path: "https://www.twitter.com",
+    id: 2,
+  },
+];
